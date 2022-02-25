18802a404f573051fe3d8e38770a9f2deff89717
jefersonrgomes <jeferson.rgomes@hotmail.com>
2022-02-24 22:36:03 -0300

    create apisMoch images

--- assets/js/api/portifolio.js
+++ assets/js/api/portifolio.js
@@ -3,49 +3,85 @@ const portifolioList = [
     title: 'Mercedes E190',
     image: './assets/img/services/machines/astra1.jpg',
     type: 'Para Brisa',
-    filter: 'filter-autos',
+    filter: 'filter-maquinas',
   },
   {
     title: 'Mercedes E190',
-    image: './assets/img/services/machines/astra1.jpg',
+    image: './assets/img/services/machines/astra2.jpg',
+    type: 'Para Brisa',
+    filter: 'filter-maquinas',
+  },
+  {
+    title: 'Mercedes E190',
+    image: './assets/img/services/machines/maq1.jpg',
+    type: 'Para Brisa',
+    filter: 'filter-maquinas',
+  },
+  {
+    title: 'Mercedes E190',
+    image: './assets/img/services/machines/maq2.jpg',
+    type: 'Para Brisa',
+    filter: 'filter-maquinas',
+  },
+  {
+    title: 'Mercedes E190',
+    image: './assets/img/services/machines/maq3.jpg',
+    type: 'Para Brisa',
+    filter: 'filter-maquinas',
+  },
+  {
+    title: 'Barco - 1',
+    image: './assets/img/services/machines/barco1.jpg',
+    type: 'Barco',
+    filter: 'filter-nautica',
+  },
+  {
+    title: 'Barco - 2',
+    image: './assets/img/services/machines/barco2.jpg',
+    type: 'Barco',
+    filter: 'filter-nautica',
+  },
+  {
+    title: 'Barco - 3',
+    image: './assets/img/services/machines/barco3.jpg',
+    type: 'Barco',
+    filter: 'filter-nautica',
+  },
+  {
+    title: 'Barco - 4',
+    image: './assets/img/services/machines/barco4.jpg',
+    type: 'Barco',
+    filter: 'filter-nautica',
+  },
+  {
+    title: 'Barco - 5',
+    image: './assets/img/services/machines/barco5.jpg',
+    type: 'Barco',
+    filter: 'filter-nautica',
+  },
+  {
+    title: 'Mercedes E190',
+    image: './assets/img/services/machines/carro1.jpg',
     type: 'Para Brisa',
     filter: 'filter-autos',
   },
   {
     title: 'Mercedes E190',
-    image: './assets/img/services/machines/astra1.jpg',
+    image: './assets/img/services/machines/carro2.jpg',
     type: 'Para Brisa',
     filter: 'filter-autos',
   },
   {
     title: 'Mercedes E190',
-    image: './assets/img/services/machines/astra1.jpg',
-    type: 'Para Brisa',
-    filter: 'filter-autos',
-  },
-  {
-    title: 'Mercedes E190',
-    image: './assets/img/services/machines/astra1.jpg',
-    type: 'Para Brisa',
-    filter: 'filter-autos',
-  },
-  {
-    title: 'Mercedes E190',
-    image: './assets/img/services/machines/astra1.jpg',
+    image: './assets/img/services/machines/carro3.jpg',
     type: 'Para Brisa',
     filter: 'filter-autos',
   },
 ];
 
-console.log(portifolioList);
-
 let idPortifolio = document.getElementById('portfolio--list');
 idPortifolio.innerHTML = PortifolioComponent(portifolioList);
-// <!-- filter-app = Super Autos -->
-// <!-- filter-web = Nautica Adventure -->
-// <!-- filter-card = Super Maqunas-->
 
-console.log(idPortifolio);
 function PortifolioComponent(e) {
   let cards = e.map((e) => {
     return `
@@ -59,9 +95,49 @@ function PortifolioComponent(e) {
               <a href=${e.image}
                 data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title=${e.title}><i
                   class="bx bx-plus"></i></a>
-              <a href="#" class="details-link" title=${e.title}><i class="bx bx-link"></i></a>
             </div>
           </div>
+      `;
+  });
+  return `${cards.join('')}`;
+}
+
+const clientesList = [
+  {
+    srcImage: 'assets/img/clients/client-1.png',
+    alt: 'Massal Guindastes',
+  },
+  {
+    srcImage: 'assets/img/clients/client-2.png',
+    alt: 'Sonho em 4 Rodas',
+  },
+  {
+    srcImage: 'assets/img/clients/client-3.png',
+    alt: 'TGL Transportes',
+  },
+  {
+    srcImage: 'assets/img/clients/client-4.png',
+    alt: 'ByDeni Studio',
+  },
+  {
+    srcImage: 'assets/img/clients/client-5.png',
+    alt: 'Batistinha Garage',
+  },
+  {
+    srcImage: 'assets/img/clients/client-6.png',
+    alt: 'Dalto Apache',
+  },
+];
+
+let idCliente = document.getElementById('client--list');
+idCliente.innerHTML = CllientComponent(clientesList);
+
+function CllientComponent(e) {
+  let cards = e.map((e) => {
+    return `
+     <div class="swiper-slide">
+         <img src=${e.srcImage} class="img-fluid" alt=${e.alt}>
+      </div>
       `;
   });
   return `${cards.join('')}`;
---
+++ assets/js/api/testemunhos.js
@@ -0,0 +1,44 @@
+const testemunhosList = [
+  {
+    client: 'Carlos Hansen',
+    company: 'Ceo &amp; Founder',
+    text: `A aparência ficou melhor do que os originais, 
+      otimo acabamento (bem lisos) sem nenhuma ondulação. 
+      Ficou perfeito!`,
+  },
+  {
+    client: 'Sara Wilsso',
+    company: 'Ceo &amp; Founder',
+    text: `Realizei a compra na ADMAQ VIDROS, 
+    que prestou um excelente atendimento, cordial, educado e cumpriu
+    com os prazos e qualidades prometidas.`,
+  },
+];
+
+console.log(testemunhosList);
+
+let idTestemunho = document.getElementById('testemunho--list');
+idTestemunho.innerHTML = TestemunhoComponent(testemunhosList);
+// <!-- filter-app = Super Autos -->
+// <!-- filter-web = Nautica Adventure -->
+// <!-- filter-card = Super Maqunas-->
+
+console.log(idTestemunho);
+function TestemunhoComponent(e) {
+   let cards = e.map((e) => {
+      return `
+      <div class="swiper-slide">
+         <div class="testimonial-item">
+            <p>
+            <img src="assets/img/quote-sign-right.png" class="quote-sign-left" alt="">
+            ${e.text} 
+            <img src="assets/img/quote-sign-right.png" class="quote-sign-right" alt="">
+            </p>
+            <h3>${e.client}</h3>
+            <h4>${e.company}</h4> 
+         </div>
+      </div>
+      `;
+   });
+   return `${cards.join('')}`;
+};
